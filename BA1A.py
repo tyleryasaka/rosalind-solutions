ad139c31ac323dde16d6a1862a68f48141fa8fac
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-21 19:37:30 -0700

    Print problem title

--- BA1A.py
+++ BA1A.py
@@ -2,6 +2,8 @@ dataset_file = open("datasets/rosalind_ba1a.txt", "r")
 t = dataset_file.readline().strip()
 p = dataset_file.readline().strip()
 
+print("Compute the Number of Times a Pattern Appears in a Text\n")
+
 def PatternCount(t, p):
     count = 0
     for i in range(len(t) - len(p) + 1):
